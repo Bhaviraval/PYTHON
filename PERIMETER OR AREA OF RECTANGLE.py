cc41b5cec60e51f74809a2d90d0aabf559100d72
Bhaviraval <24bit003@sot.pdpu.ac.in>
2025-01-21 15:32:17 +0530

    Add files via upload

---
+++ PERIMETER OR AREA OF RECTANGLE.py
@@ -0,0 +1,9 @@
+def perimeter():
+    a=int(input("enter the length"))
+    b=int(input("enter the breadth"))
+    if 2*(a+b)>a*b:
+        print("the perimeter of rectangle is greater")
+    else:
+        print("the rectangle is greater than the perimeter")
+perimeter()
+              
